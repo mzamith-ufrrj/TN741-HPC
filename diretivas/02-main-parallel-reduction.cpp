ea51ce85161c4394de55a7fec55959db2729a3b9
mzamith-ufrrj <mzamith@ufrrj.br>
2024-09-22 19:50:12 -0300

    Atualização de exemplo - 22/09/2024

--- diretivas/02-main-parallel-reduction.cpp
+++ diretivas/02-main-parallel-reduction.cpp
@@ -8,38 +8,40 @@
 #include <climits>
 #include <omp.h>
 #include <unistd.h>
-#include <unistd.h>
+#include <random>
 using namespace std;
 #define ALING 64
 
-
 int main (int ac, char **av){
-    unsigned long long sum = 0;
-    unsigned long *vet = NULL,
-                  size = strtoul(av[1], NULL, 0);
-
+    unsigned long size = strtoul(av[1], NULL, 0);
+    double *vet  = NULL;
     unsigned int nThreads = stoi(av[2]);
+    //std::mt19937_64  generator (time(nullptr)); //64 bits
+    std::mt19937_64  generator (42); //64 bits
+    uniform_real_distribution<double> unif(0, 1); //uniform distribuition
 
     cout << endl << "\tRedução" << endl;
-    size *= 1048576 / sizeof(unsigned long);
-    cout << "               Tamanho:" << (size * sizeof(unsigned long)) / 1048576 << " em Mbytes " << endl;
+    size *= 1048576 / sizeof(double);
+    cout << "               Tamanho:" << (size * sizeof(double)) / 1048576 << " em Mbytes " << endl;
     cout << "              Posições:" << size << endl;
 
-    posix_memalign(reinterpret_cast <void**>(&vet), ALING, size * sizeof(unsigned long));
-    omp_set_num_threads(nThreads);
+    assert(posix_memalign(reinterpret_cast <void**>(&vet), ALING, size * sizeof(double)) == 0);
 
-    //#pragma omp parallel for
-    for (unsigned long i = 0; i < size; i++){
-        vet[i] = i + 1;
+
+    #pragma omp parallel for
+    for (unsigned long i = 1; i < size; i++){
+        vet[i] = unif(generator) * 100.0f;
     }
 
-    omp_set_num_threads(nThreads);
+
+
+    double sum = 0.0f;
     #pragma omp parallel for shared(size, vet) reduction(+:sum)
     for (unsigned long i = 0; i < size; i++){
-        sum += static_cast<unsigned long long> (vet[i]);
+        sum += vet[i];
     }
 
-    cout << "Resultado da soma: " <<  sum << endl;
+    cout << "Valor somado: " << sum  << endl;
 
 
     free(vet);
